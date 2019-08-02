696d48cdbb1a5a66a48785c3e9a316d4bc9d866f
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-08-02 15:02:53 +0200

    Added component TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function TodoList() {
-  return <ul />;
+export default function TodoList({ todos, onTodoClick }) {
+  return (
+    <ul>
+      {todos.map((todo, index) => {
+        <Todo key={index} {...todo} onClick={() => onTodoClick(index)} />;
+      })}
+    </ul>
+  );
 }
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  ontodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList;
