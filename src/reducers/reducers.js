5bd202d71d7ea9dbbf74545c21759c0e04c31687
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-08-02 12:35:43 +0200

    Cleaned up reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,3 @@
-export default (todoApp = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-});
-
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
   todos: []
