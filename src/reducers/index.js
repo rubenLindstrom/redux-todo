07d754babc8530a529b889509b1fc438ca90072f
Ruben Lindström <ruben.lindstrom@gmail.com>
2019-08-02 13:47:44 +0200

    Finalized reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,18 @@
 //NOTE: Reducers specify how the application's state changes in repsonse to actions sent to the store. Distinction: Actions describe WHAT happened, not how it affects the application
 
+import { combineReducers } from "redux";
+
 import { VisibilityFilters } from "../constants";
-import todoReducer from "./todoReducer";
-import visibilityReducer from "./visibilityReducer";
+import { todoReducer as todos } from "./todoReducer";
+import { visibilityReducer as visibilityFilter } from "./visibilityReducer";
 
 const initialState = {
   visibilityFilter: VisibilityFilters.SHOW_ALL,
   todos: []
 };
 
-function todoApp(state = initialState, action) {
-  return {
-    visibilityFilter: visibilityReducer(state.visibilityFilter, action),
-    todos: todoReducer(state.todos, action)
-  };
-}
+// för att använda denna syntax är det viktigt att reducern har samma namn som den slice av state den hanterar.
+export default combineReducers({
+  visibilityFilter,
+  todos
+});
